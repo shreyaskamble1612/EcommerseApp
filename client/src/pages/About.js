50ba84e364bb04bd8b4a067bbedb4166f181d0a5
PiyushJadhav0604 <piyushkrishna2004@gmail.com>
2024-08-04 03:52:41 +0530

    Routing

---
+++ client/src/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+const About = () => {
+  return (
+    <Layout><h1>About</h1></Layout>
+  )
+}
+
+export default About
---
+++ client/src/pages/Pagenotfound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+const Pagenotfound = () => {
+  return (
+    <Layout><h1>Paagenotfound</h1></Layout>
+  )
+}
+
+export default Pagenotfound
